b634a5acc1966e46a102a0f7f3b9dc08285bc781
Gustvo Paniza <panizagustavo@gmail.com>
2022-11-09 17:00:15 -0500

    redireccion temporal del index a perfil.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistemas de Reportes Innova</title>
 
-  <meta http-equiv="Refresh" content="0;url=pages/home.php">
+  <!-- <meta http-equiv="Refresh" content="0;url=pages/home.php"> -->
+  <meta http-equiv="Refresh" content="0;url=pages/perfil.php">
 </head>
 </html>
